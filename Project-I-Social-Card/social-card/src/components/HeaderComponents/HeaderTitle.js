1f690c4c6b6722b88a47f49874b938ff29f04d0a
unknown <stanj89@gmail.com>
2019-05-28 17:26:39 -0400

    header js complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h1>Lambda School</h1>
+      <h2>@LambdaSchool</h2>
+      <ul>
+        <li>26 Jan</li>
+      </ul>
+    </div>
+  );
+};
+
+export default HeaderTitle;
